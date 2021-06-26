ac8d8f631d724dbd8fbd5d55d54752734a03dbae
vishnu92karthik <vishnumkarthik@gmail.com>
2021-06-26 16:25:02 +0530

    React

--- biased-bacon/App.js
+++ biased-bacon/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import {View,Text} from 'react-native';
 
+const Profile = ({name,status})=>{
+  
+  return(
+    <View>
+    <Text>Vishnu is a bodyBuilder</Text>
+    <Text>{name}</Text>
+     <Text>{status}</Text>
+    </View>
+  )
+}
+
 function App(){
-  return <Text> Hello vishnu </Text>
+  return ( 
+    <View>
+  <Profile name="karthik" status="single"/>
+  <Profile name="vishnu" status="programmer"/>
+  <Profile name="Asok" status="committed"/>
+  
+    </View>
+    )
 }
 
 export default App;
